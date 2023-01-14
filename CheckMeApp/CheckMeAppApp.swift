1feb6ff3d7030868d94e8a087c580c373f40a7ab
LinaAlmusfir <LinaAlmusfir@gmail.com>
2023-01-14 17:58:55 +0300

    Initial Commit

---
+++ CheckMeApp/CheckMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeAppApp.swift
+//  CheckMeApp
+//
+//  Created by Lina on 21/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
